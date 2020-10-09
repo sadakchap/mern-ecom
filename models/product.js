41693567a06a259adf756a417e4815fd572f2dad
sadakchap <aliceprerna@gmail.com>
2020-10-09 17:23:47 +0530

    created Product Schema

---
+++ models/product.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    desc: {
+        type: String,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        maxlength: 32
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    },
+    stock: {
+        type: Number
+    },
+    sold:{
+        type: Number,
+        default: 0
+    },
+    photo: {
+        type: Buffer,
+        contentType: String
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Product", productSchema);
